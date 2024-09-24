885cf9b7c420b0cd96dc9ca3f8e779ae046ab870
Harshit Sohaney <73911621+HarshitSohaney@users.noreply.github.com>
2024-09-24 15:45:52 -0400

    Chapter 4 - (part 3) Added email verfication

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Regexp
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -19,15 +19,19 @@ def index():
 
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
 
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index.html', 
                             form=form, 
-                            name=session.get('name'), 
-                            current_time=datetime.utcnow())
+                            name=session.get('name'),
+                            email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -43,4 +47,11 @@ def internal_server_error(e):
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[
+                                DataRequired(),
+                                Email(),
+                                Regexp(r'^[a-zA-Z]+\.[a-zA-Z]+@mail\.utoronto\.ca$', 
+                                    message='Please enter a valid UofT email with first and last name')
+                                ]
+                        )
     submit = SubmitField('Submit')
