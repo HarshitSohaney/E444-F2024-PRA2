d3135910fa37c97000d10756f85043a54910ea8c
Harshit Sohaney <73911621+HarshitSohaney@users.noreply.github.com>
2024-09-24 15:19:39 -0400

    Chapter 4 - (part 1) Add a form and include user sessions

--- hello.py
+++ hello.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'who could possibly guess this?absdexyz'
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('/index.html', 
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', 
+                            form=form, 
+                            name=session.get('name'), 
                             current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
@@ -23,3 +37,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('/500.html'), 500
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
