e483e0b9c422dd5aa61697527836017e7dced017
Harshit Sohaney <73911621+HarshitSohaney@users.noreply.github.com>
2024-09-26 15:18:54 -0400

    Fix email validation

--- hello.py
+++ hello.py
@@ -4,8 +4,8 @@ from flask_moment import Moment
 from datetime import datetime
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email, Regexp
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, Regexp, ValidationError
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -47,12 +47,12 @@ def internal_server_error(e):
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    email = StringField('What is your UofT Email address?', validators=[
-                                DataRequired(),
-                                Email(),
-                                Regexp(r'^[a-zA-Z]+\.[a-zA-Z]+@mail\.utoronto\.ca$', 
-                                    message='Please enter a valid UofT email with first and last name')
-                                ]
+    email = EmailField('What is your UofT Email address?', validators=[
+                            DataRequired(),
+                            Email(),
+                            Regexp(r'^[a-zA-Z]+\.[a-zA-Z]+@mail\.utoronto\.ca$', 
+                                message='Please enter a valid UofT email with first and last name')
+                            ]
                         )
     submit = SubmitField('Submit')
 
